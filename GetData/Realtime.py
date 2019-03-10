302e6779d71cbbbb2278218102a47521136704b8
刘宇翔 <robin@unidax.com>
2019-03-10 15:24:47 +0800

    up

--- GetData/Realtime.py
+++ GetData/Realtime.py
@@ -87,15 +87,15 @@ def Run(coinParam):
         _ratioP = (_newBase - _basePrice) / _basePrice
         _ratioV = (_newBaseVol - _baseVol) / _baseVol
         #print('2* %s%s' % (_ratioP, _ratioV))
-        pprint('base_ratio=%s' % (_ratioP * 100))
+        print('base_ratio=%s' % (_ratioP * 100))
 
         # 根据价格范围 调整价格涨跌
         # 使用反正切函数控制
         NewPrice = _lastPrice * (1 + _ratioP)
-        pprint('price=%s' % (NewPrice))
+        print('price=%s' % (NewPrice))
 
         NewPrice = get_final_price_atan(_priceUp, _priceLow, NewPrice)
-        pprint('after_price=%s' % (NewPrice))
+        print('after_price=%s' % (NewPrice))
 
         NewVol = _lastVol * (1 + _ratioV)
 
